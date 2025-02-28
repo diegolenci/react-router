1b7fa2a9cef5ea41f147760424e2f6605896066a
diegolenci <lencidiego0@gmail.com>
2025-02-28 16:46:41 +0100

    Add: component contactPage

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import HomePage from "./pages/HomePage"
+import ContactPage from "./pages/ContactPages"
 
 function App() {
 
@@ -8,6 +9,7 @@ function App() {
       <BrowserRouter>
       <Routes>
         <Route path="/" Component={HomePage}/>
+        <Route path="/contact" Component={ContactPage}/>
       </Routes>
       </BrowserRouter>
     </>
---
+++ src/pages/ContactPages.jsx
@@ -0,0 +1,7 @@
+const ContactPage = () => {
+    return(
+        <h1>Chi siamo</h1>
+    )
+}
+
+export default ContactPage
