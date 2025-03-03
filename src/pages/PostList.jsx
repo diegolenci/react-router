55ba219d30377b97223f875c99909e55d27dde2f
diegolenci <lencidiego0@gmail.com>
2025-03-03 15:28:20 +0100

    Add: link lista dei posts con axios

--- src/pages/PostList.jsx
+++ src/pages/PostList.jsx
@@ -1,10 +1,37 @@
 import MainNav from "../components/MainNav"
+import { useState, useEffect } from "react"
+import axios from "axios"
 
 const PostList = () => {
+
+    const [ posts, setPosts ] = useState([])
+
+    useEffect( () => {
+        axios.get('http://localhost:3000/api/posts')
+        .then( res => setPosts(res.data))
+    }, [] )
     return(
         <>
             <MainNav/>
-            <h1>Lista post</h1>
+            <div className="container">
+                <div className="row">
+                    {
+                        posts.map((elem) => {
+                            return(
+                                <div key={elem.id} className="col-4">
+                                    <div className="card">
+                                        {/*<img className="card-img-top" src="" alt="" />*/}
+                                        <div className="card-body">
+                                            <h4 className="card-title">{elem.title}</h4>
+                                            <p className="card-text">{elem.content}</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            )
+                        })
+                    }
+                </div>
+            </div>
         </>
     )
 }
